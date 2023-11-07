1aa5e002653b4609f2778d94a9f543d9a86793ce
Manuel Ortega Ruiz <manuellortegaruiz@gmail.com>
2023-11-07 10:04:00 -0500

    API Initial setup, firebase config and litner

---
+++ firebase-config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCWFliVbq7k81Eydbzhb6V2QBrPknyYYPE',
+  authDomain: 'relatanosapp.firebaseapp.com',
+  projectId: 'relatanosapp',
+  storageBucket: 'relatanosapp.appspot.com',
+  messagingSenderId: '818104002738',
+  appId: '1:818104002738:web:e9b9d6794e2b8518f681ab',
+};
